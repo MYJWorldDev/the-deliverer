a286ddf7409c3e338a7643959b87c59b486f4f79
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2025-08-14 13:05:40 +0300

    Update pages

--- App/TheDeliverer/composeApp/src/wasmJsMain/kotlin/com/myjworld/thedrone/main.kt
+++ App/TheDeliverer/composeApp/src/wasmJsMain/kotlin/com/myjworld/thedrone/main.kt
@@ -3,10 +3,19 @@ package com.myjworld.thedrone
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.w3c.dom.HTMLElement
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    removeLoadingSpinner()
     ComposeViewport(document.body!!) {
         App()
     }
+}
+
+fun removeLoadingSpinner() {
+    val spinnerImg = document.getElementById("loading-logo") as? HTMLElement
+    spinnerImg?.style?.display = "none"
+    val spinner = document.getElementById("loading-spinner") as? HTMLElement
+    spinner?.style?.display = "none"
 }
